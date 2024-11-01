71b79e9b0daacb7c7e71528798b8cf6dc73926a7
AdrianFerreira7 <dev.adrianferreira@gmail.com>
2024-10-31 21:37:34 -0300

    Commit Sanity

--- schemaTypes/MissaoValoresType.js
+++ schemaTypes/MissaoValoresType.js
@@ -2,7 +2,7 @@ import {defineField, defineType} from 'sanity'
 
 export const MissaoValoresType = defineType({
   name: 'Missao e Valores',
-  title: 'Event',
+  title: 'Missao e Valores',
   type: 'document',
   fields: [
     defineField({
