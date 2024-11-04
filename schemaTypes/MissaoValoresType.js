2896535661aee518488a27c38252b0c126b888e5
AdrianFerreira7 <dev.adrianferreira@gmail.com>
2024-11-04 19:30:14 -0300

    first commit

--- schemaTypes/MissaoValoresType.js
+++ schemaTypes/MissaoValoresType.js
@@ -2,7 +2,7 @@ import {defineField, defineType} from 'sanity'
 
 export const MissaoValoresType = defineType({
   name: 'Missao e Valores',
-  title: 'Missao e Valoreaagits',
+  title: 'Missao e Valoreaagitas',
   type: 'document',
   fields: [
     defineField({
